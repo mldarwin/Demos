f64c8d67a88279480d343e04c37c3660509c3284
Marielle Darwin <78009407+mldarwin@users.noreply.github.com>
2021-11-18 23:09:20 -0700

    Read in unfiltered ephys channel data

--- channRead.m
+++ channRead.m
@@ -1,22 +1,23 @@
 % ---Function 'channRead'---
-% Read in single channel of microwire data from NWB file
-% Inputs flexible to specify subject, session, channel, and BasePath
+% Read in single channel of unfiltered NLX data from NWB file
 % 
-% Channel inputs are 1-8 for MW channels 257-264 respectively
-% BasePath is the location of the folder that conatins all patient folders
-%     E.g.-> C:\Users\darwinm\Documents\Thompson Lab\Microwire\PatientData\ 
-%
-% Marielle L. Darwin | October 7 2021
+% channel input = 1-8 for MW channels 257-264 respectively
+% basePath input = location of the folder that conatins all patient folders
+% wireType input = 1 for macro data, 2 for micro data
+% 
+% Example inputs: 
+% subjectID='MW5';
+% session='3';
+% channel=1;
+% basePath='C:\Users\darwinm\Documents\Thompson Lab\Microwire\PatientData\';
+% wireType=1;
+% Marielle L. Darwin | October 7 2021 | Last revision: November 18 2021
 
-
-function [MWchannel]=channRead(subjectID,session,channel,BasePath)
-
-% Convert inputs to string variables
-session = num2str(session);
-subjectID = num2str(subjectID);
+function [MWchannel, Fs, channelData, refTS] = channRead(subjectID, ...
+    session, channel, basePath, wireType)
 
 % Location of subject folder
-subjectFolder = insertAfter(BasePath,length(BasePath),subjectID);
+subjectFolder = insertAfter(basePath,length(basePath),subjectID);
 
 % Recreate NWB file name from input arguments
     % Insert subject ID into file name
@@ -27,14 +28,14 @@ subjectFolder = insertAfter(BasePath,length(BasePath),subjectID);
     newStr2 = insertAfter(str2,12,session);
 
 % Location of subject NWB subfolder
-filePath = append(subjectFolder, "\NWB_Data");
+filePath = append(subjectFolder,"\NWB_Data");
 
 % Read in NWB
 cd(filePath);
 mwNWB = nwbRead(newStr2);
 
 % Pop up to browse contents of NWB file
-util.nwbTree(mwNWB)
+%util.nwbTree(mwNWB)
 
 % Extract relevant NWB info
     % Handle for general electrophysiology - both macro and micro
@@ -43,7 +44,23 @@ util.nwbTree(mwNWB)
     vecInfo = genEphys.vectordata;
     % Handle for channel number ID
     channEl = vecInfo.get('channID').data.load();
+    % Handle for timestamps
+    refTS = mwNWB.timestamps_reference_time;
 
-% Specify channel
+% Specify channel number
 MWchannel=channEl(channel);
+
+% Sampling frequency
+    % Access the struct names in the NWB acquisition field 
+    unit_names = keys(mwNWB.acquisition);
+    mwireData = mwNWB.acquisition.get(unit_names{wireType});
+    
+Fs = mwireData.starting_time_rate;
+
+% Extract channel data
+    % Extract vector of data
+    ALLchannelData = mwireData.data.load();
+
+    % Extract data from channel of interest
+    channelData = ALLchannelData(channel,:);
 end
